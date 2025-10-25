6bd437d46704f8020d4e89a3374d9d1e85398938
premvarma2002 <vakilverma1982@gmail.com>
2025-10-25 20:31:43 +0530

    setting up clerk auth

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+
+const header = () => {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+    </div>
+  )
+}
+
+export default header
