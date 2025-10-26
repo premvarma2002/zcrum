24a5778e124217b4f82808bed1fa8056b7543fc7
premvarma2002 <vakilverma1982@gmail.com>
2025-10-26 21:03:02 +0530

    clerk theme added

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import {Inter} from 'next/font/google';
+import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Zcrum",
@@ -13,22 +14,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      > 
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        <Header/>
-        <main className="min-h-screen">{children}</main>
-        <footer className="bg-gray-900 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-          <p>Made with ❤️ by prem</p>
-          </div>
-        </footer>
-      </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      loadStrategy="lazy"
+      appearance={{
+        baseTheme: shadesOfPurple,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with ❤️ by Prem</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
