9d7a4a0c234bdd405bffd5dd2c613065e4cdaa8f
premvarma2002 <vakilverma1982@gmail.com>
2025-11-04 19:57:03 +0530

    Companies section added

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Activity, ChevronRight, Layout, Users } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import CompanyCarousel from "@/components/company-carousel";
 import { Card, CardContent } from "@/components/ui/card";
 
 const features = [
@@ -74,6 +75,12 @@ export default function Home() {
               </Card>
             ))}
           </div>
+        </div>
+      </section>
+       <section className="py-20">
+        <div className="container mx-auto">
+          <h3 className="text-3xl font-bold mb-12 text-center">Trusted by Industry Leaders</h3>
+          <CompanyCarousel />
         </div>
       </section>
     </div>
